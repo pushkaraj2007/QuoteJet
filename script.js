31f55760728d75cd5a83d03b533354aa02eb3763
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2022-08-02 19:37:49 +0530

    Added responsiveness to quote generating section

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ const generateQuoteBtn = document.querySelector('.generate-quote-btn');
 const quote = document.querySelector('.quote');
 const quoteAuthor = document.querySelector('.quote-author');
 
+const generateQuote = async ()=>{
+    let res = await fetch('https://api.quotable.io/random');
+    let response = await res.json()
+    quote.innerText = `"${response.content}"`
+    quoteAuthor.innerText = `- ${response.author}`
+}
+
+generateQuote();
+
 generateQuoteBtn.onclick = async ()=>{
     try {
-        let res = await fetch('https://api.quotable.io/random');
-        let response = await res.json()
-        quote.innerText = `"${response.content}"`
-        quoteAuthor.innerText = `- ${response.author}`
-        console.log(response)
+       await generateQuote()
     } catch (error) {
         console.log(error)
     }
